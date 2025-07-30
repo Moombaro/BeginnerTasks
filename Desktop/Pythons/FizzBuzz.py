869759040b38e0680991355fa3ed64d3577ad497
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 14:55:10 +0200

    print objective and create a list up to 100

--- Desktop/Pythons/FizzBuzz.py
+++ Desktop/Pythons/FizzBuzz.py
@@ -1,1 +1,7 @@
+print("Object is to create a List 1 - 100, and replace multiples of 3 by 'Fizz', multiples of 5 by 'Buzz' and if they collide 'FizzBuzz'.")
 
+
+x = 1
+for myList in range(1,101):
+
+    print(myList)
