2056a812881222efd3421d6579108408ceb4c500
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 12:34:08 +0200

    added try and exceptions to function, also createt calculator function

--- Desktop/Pythons/Calc.py
+++ Desktop/Pythons/Calc.py
@@ -20,28 +20,36 @@ def root(x):
 def root(y):
     return math.sqrt(y)
 
+def Calculator():
+    try:
+        x = float(input("First number: "))
+        y = float(input("Second number: "))
+        userChoice = str(input("What would you like to do (addtion, subtraction, multiplication, division, exponential, root or dividible)? "))
+    
 
-x = float(input("First number: "))
-y = float(input("Second number: "))
-userChoice = input("What would you like to do (addtion, subtraction, multiplication, division, exponential, root or dividible)? ")
+        if userChoice == "addition":
+            print(x, "+", y, "=", addition(x, y))
+        elif userChoice == "subtraction":
+            print(x, "-", y, "=", subtraction(x, y))
+        elif userChoice == "multiplication":
+            print(x, "*", y, "=", multiplication(x, y))
+        elif userChoice == "division":
+            print(x, "/", y, "=", division(x, y))
+        elif userChoice == "exponential":
+            print(x, "^", y, "=", exponential(x, y))
+        elif userChoice == "dividible":
+            print(dividible(x, y))
+        elif userChoice == "root":
+            print("Root of ", x, "=", root(x))
+            print("Root of ", y, "=", root(y))
+        else:
+            print("invalid order")
+    except ValueError and ZeroDivisionError:
+        return print("invalid input")
 
-if userChoice == "addition":
-    print(x, "+", y, "=", addition(x, y))
-elif userChoice == "subtraction":
-    print(x, "-", y, "=", subtraction(x, y))
-elif userChoice == "multiplication":
-    print(x, "*", y, "=", multiplication(x, y))
-elif userChoice == "division":
-    print(x, "/", y, "=", division(x, y))
-elif userChoice == "exponential":
-    print(x, "^", y, "=", exponential(x, y))
-elif userChoice == "dividible":
-    print(dividible(x, y))
-elif userChoice == "root":
-    print("Root of ", x, "=", root(x))
-    print("Root of ", y, "=", root(y))
-else:
-    print("invalid order")
+
+Calculator()
+
 
 
 
