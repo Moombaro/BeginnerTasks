a0214719abd979dde39a16659e8c29a907da3c01
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 11:22:34 +0200

    ask for choice

--- Desktop/Pythons/Calc.py
+++ Desktop/Pythons/Calc.py
@@ -6,3 +6,7 @@ def multiplication(x, y):
     return x * y
 def division(x, y):
     return x / y
+def exponential(x, y):
+    return x ** y
+
+userChoice = input("What would you like to do (addtion, subtraction, multiplication, division or exponential)? ")
