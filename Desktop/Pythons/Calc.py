6ad5ac9eefb2fdad38b3512ca5d10a0a1fdb2bb9
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 11:40:34 +0200

    using if elif else to print chosen results

--- Desktop/Pythons/Calc.py
+++ Desktop/Pythons/Calc.py
@@ -9,4 +9,23 @@ def division(x, y):
 def exponential(x, y):
     return x ** y
 
+x = float(input("First number: "))
+y = float(input("Second number: "))
 userChoice = input("What would you like to do (addtion, subtraction, multiplication, division or exponential)? ")
+
+if userChoice == "addition":
+    print(addition(x, y))
+elif userChoice == "subtraction":
+    print(subtraction(x, y))
+elif userChoice == "multiplication":
+    print(multiplication(x, y))
+elif userChoice == "division":
+    print(division(x, y))
+elif userChoice == "exponential":
+    print(exponential(x, y))
+else:
+    print("invalid order")
+
+
+
+
