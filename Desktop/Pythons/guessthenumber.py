c1831cd8f3a910a38ef047aadf7f42a3a0c6a404
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 15:33:48 +0200

    added a while loop to keep game going till the end

--- Desktop/Pythons/guessthenumber.py
+++ Desktop/Pythons/guessthenumber.py
@@ -1,11 +1,16 @@
 import random
 
 number_to_guess = random.randint(1, 101)
-user_guess = int(input("Guess a number between 1 and 100: "))
-if user_guess < number_to_guess:
-    print("To low")
-elif user_guess > number_to_guess:
-    print("To high")
-else:
-    print("Perfect")
-    
+guess = False
+
+while not guess:
+    user_guess = int(input("Guess a number between 1 and 100: "))
+    if user_guess < number_to_guess:
+        print("To low")
+    elif user_guess > number_to_guess:
+        print("To high")
+    else:
+        guess = True
+        print("Perfect")
+       
+
