48caa2a273592060acb8bab285f927c35ada3d49
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 15:16:25 +0200

    simple if/else odd-even checker by using modulo

---
+++ Desktop/Pythons/oddeven.py
@@ -0,0 +1,10 @@
+print("Welcome to the odd/even check!")
+
+number = int(input("Please choose your number: "))
+
+
+if number % 2 == 1:
+    print("Your number is odd.")
+else:
+    print("Your number is even.")
+
