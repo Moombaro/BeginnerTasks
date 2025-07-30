2b16e4b216cdbb9c351c1d66351b044d1ecafdaa
Moombaro <roland.sauerbier2@googlemail.com>
2025-07-30 15:28:11 +0200

    set conditions for guessed numbers

--- Desktop/Pythons/guessthenumber.py
+++ Desktop/Pythons/guessthenumber.py
@@ -1,23 +1,11 @@
 import random
 
-number_to_guess = random.randint(1, 100)
-found = False
-
-while not found:
-    user_guess = input("whats my number: ")
-
-    if int(user_guess) < number_to_guess:
-        print("to low")
-    elif int(user_guess) > number_to_guess:
-        print("to high")
-    else: 
-        found = True
-        print("HIT")
-
-
-
+number_to_guess = random.randint(1, 101)
+user_guess = int(input("Guess a number between 1 and 100: "))
+if user_guess < number_to_guess:
+    print("To low")
+elif user_guess > number_to_guess:
+    print("To high")
+else:
+    print("Perfect")
     
-
-
-
-
